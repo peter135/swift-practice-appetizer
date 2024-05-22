2b9783bf9a5feb0b6847d0e7519274e16d4ccd35
wangjian <wangjian@eazytec.com>
2024-05-22 16:59:45 +0800

    Initial Commit

---
+++ Appetizer/AppetizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizerApp.swift
+//  Appetizer
+//
+//  Created by apple on 2024/5/22.
+//
+
+import SwiftUI
+
+@main
+struct AppetizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
